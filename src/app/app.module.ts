2bd192358ebbd9b6359eac29f4dfb68df6d828fd
Roman <7243180roma65@gmail.com>
2022-03-23 20:22:57 +0300

    homework-2
    TS ругался на типы данных.
    Все погуглил, проблему решил)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 @NgModule({
   declarations: [
     AppComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+
+export class CardComponent implements OnInit {
+
+  @Input()name!: string;
+  @Input()number!: string;
+  @Input()amount!: string;
+  @Input()expirationDate!: string;
+  @Input()paymentSystem!: 'visa' | 'mastercard';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
