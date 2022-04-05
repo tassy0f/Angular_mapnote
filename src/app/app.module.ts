54add1717ece7a030f1cf0a5fa99d2037be35b3f
Roman <7243180roma65@gmail.com>
2022-04-05 17:26:06 +0300

    homework-5
    hw-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { AttrDirective } from './attr-directive.directive';
 import { LocateCurrencyPipe } from './locateCurrency.pipe';
+import { TimerService } from './timer.service';
+import { DOCUMENT, SET_INTERVAL } from './token';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,9 +16,20 @@ import { LocateCurrencyPipe } from './locateCurrency.pipe';
     LocateCurrencyPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    TimerService,
+    {
+      provide: DOCUMENT,
+      useValue: document,
+    },
+    {
+      provide: SET_INTERVAL,
+      useValue: setInterval,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
